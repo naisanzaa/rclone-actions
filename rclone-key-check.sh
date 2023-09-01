901303beb73e4cc75aa97a7d537166437b70964b
Eric <58240560+naisanzaa@users.noreply.github.com>
2023-09-01 06:47:43 -0700

    Update rclone-key-check.sh

--- rclone-key-check.sh
+++ rclone-key-check.sh
@@ -2,14 +2,14 @@
 
 # check if rclone accounts are still valid
 
+RCLONE="rclone --config rclone.conf"
+
 if [ -f errors.log ]; then rm -fv errors.log; fi 
 
-for remote in $(rclone listremotes); do 
-  if rclone lsd "$remote" >/dev/null 2>>errors.log; then 
+for remote in $(${RCLONE} listremotes); do
+  if ${RCLONE} lsd "$remote" >/dev/null 2>>errors.log; then
     echo "OK $remote"
   else 
     echo "FAIL $remote"
   fi 
 done
-
-
