b3f4254d9ae969caa439c8af20c85c660f22d8ec
Eric <58240560+naisanzaa@users.noreply.github.com>
2023-09-01 06:47:53 -0700

    Update rclone-lsd.sh

--- rclone-lsd.sh
+++ rclone-lsd.sh
@@ -2,15 +2,15 @@
 
 # check list dirs
 
+RCLONE="rclone --config rclone.conf"
+
 if [ -f logs.log ]; then rm -v logs.log; fi 
 
-for remote in $(rclone listremotes); do 
-  if rclone lsd "$remote" >/dev/null; then 
+for remote in $(${RCLONE} listremotes); do
+  if ${RCLONE} lsd "$remote" >/dev/null; then
     echo "OK $remote" | tee -a logs.log
-    rclone lsd "$remote" >> logs.log
+    ${RCLONE} lsd "$remote" >> logs.log
   else 
     echo "FAIL rclone lsd $remote"
   fi 
 done
-
-
