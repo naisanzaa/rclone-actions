faf3e655c94ccb71f0a7875becb6f9da676222a2
naisanzaa <naisanza@gmail.com>
2023-09-02 14:57:46 +0900

    update runs

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ RCLONE="rclone --config rclone.conf"
 ${RCLONE} listremotes
 
 # eric
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories     >> /dev/null
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life  >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   wasabi:archive-eric                >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   naisanza-onedrive:home/eric        >> /dev/null 2>&1
+${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories     >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life  >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   wasabi:archive-eric                >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   naisanza-onedrive:home/eric        >> /dev/null 2>&1
 
 # dad
-${RCLONE} sync -v --delete-before dad-onedrive:  wasabi:archive-dad/onedrive         >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive           >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before dad-onedrive:  wasabi:archive-dad/onedrive         >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive           >> /dev/null 2>&1
 
 # chaofamily
-${RCLONE} sync -v --delete-before chaofamily-gdrive:family       wasabi-eu:archive-family          >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance      >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before chaofamily-gdrive:family       chaofamily-photos:album/family    >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before chaofamily-gdrive:family       wasabi-eu:archive-family          >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance      >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before chaofamily-gdrive:family       chaofamily-photos:album/family    >> /dev/null 2>&1
