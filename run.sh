b9a1253722be0ce3186a35dc8813db41072094d2
naisanzaa <naisanza@gmail.com>
2023-09-02 15:00:21 +0900

    update runs

--- run.sh
+++ run.sh
@@ -14,8 +14,8 @@ ${RCLONE} listremotes
 #${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   naisanza-onedrive:home/eric        >> /dev/null 2>&1
 
 # dad
-#${RCLONE} sync -v --delete-before dad-onedrive:  wasabi:archive-dad/onedrive         >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive           >> /dev/null 2>&1
+${RCLONE} sync -v --delete-before dad-onedrive:  wasabi:archive-dad/onedrive         >> /dev/null 2>&1
+#${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive           >> /dev/null 2>&1
 
 # chaofamily
 #${RCLONE} sync -v --delete-before chaofamily-gdrive:family       wasabi-eu:archive-family          >> /dev/null 2>&1
