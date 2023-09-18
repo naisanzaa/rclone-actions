80d23956191fa044e1117f66326fcb098047e575
naisanzaa <naisanza@gmail.com>
2023-09-19 01:38:52 +0900

    debugging

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash 
 
-set -e
+set -ex
 
 RCLONE="rclone --config rclone.conf"
 
 # add rclone commands here
 
 # eric
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories     >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life  >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   wasabi:archive-eric                >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   naisanza-onedrive:home/eric        >> /dev/null 2>&1
+${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories       2>&1 | grep '%' || echo $?
+${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life    2>&1 | grep '%' || echo $?
+${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   wasabi:archive-eric                  2>&1 | grep '%' || echo $?
+${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   naisanza-onedrive:home/eric          2>&1 | grep '%' || echo $?
 
 # dad
-${RCLONE} sync -v --delete-before dad-onedrive:  wasabi:archive-dad/onedrive         >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive           >> /dev/null 2>&1
+${RCLONE} sync -v --delete-before dad-onedrive:  wasabi:archive-dad/onedrive                                                 2>&1 | grep '%' || echo $?
+${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive                                                   2>&1 | grep '%' || echo $?
 
 # chaofamily
-${RCLONE} sync -v --delete-before chaofamily-gdrive:family       wasabi-eu:archive-family          >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance      >> /dev/null 2>&1
-${RCLONE} sync -v --delete-before chaofamily-gdrive:family       chaofamily-photos:album/family    >> /dev/null 2>&1
+${RCLONE} sync -v --delete-before chaofamily-gdrive:family       wasabi-eu:archive-family                                    2>&1 | grep '%' || echo $?
+${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance                                2>&1 | grep '%' || echo $?
+${RCLONE} sync -v --delete-before chaofamily-gdrive:family       chaofamily-photos:album/family                              2>&1 | grep '%' || echo $?
