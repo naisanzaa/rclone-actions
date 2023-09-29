d7b4310364d12ecee17facc40aacc1a9131a2508
naisanzaa <naisanza@gmail.com>
2023-09-29 16:36:40 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ RCLONE="rclone --config rclone.conf"
 
 # eric
 ${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories       2>&1 | grep '%'
-
+${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life    2>&1 | grep '%'
 ${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   wasabi:archive-eric                  2>&1 | grep '%'
 ${RCLONE} sync -v --delete-before --drive-acknowledge-abuse naisanza-gdrive:home/eric   naisanza-onedrive:home/eric          2>&1 | grep '%'
 
@@ -20,6 +20,3 @@ ${RCLONE} sync -v --delete-before dad-gdrive:    wasabi:archive-dad/gdrive      
 ${RCLONE} sync -v --delete-before chaofamily-gdrive:family       wasabi-eu:archive-family                                    2>&1 | grep '%'
 ${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance                                2>&1 | grep '%'
 ${RCLONE} sync -v --delete-before chaofamily-gdrive:family       chaofamily-photos:album/family                              2>&1 | grep '%'
-
-# eric
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life    2>&1 | grep '%'
