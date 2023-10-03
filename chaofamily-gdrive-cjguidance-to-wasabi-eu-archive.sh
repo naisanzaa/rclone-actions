05d029b45a16939dfa3b0c55a96cafb9d74327e8
Eric <58240560+naisanzaa@users.noreply.github.com>
2023-10-03 16:00:42 +0800

    Update chaofamily-gdrive-cjguidance-to-wasabi-eu-archive.sh

--- chaofamily-gdrive-cjguidance-to-wasabi-eu-archive.sh
+++ chaofamily-gdrive-cjguidance-to-wasabi-eu-archive.sh
@@ -6,4 +6,4 @@ RCLONE="rclone --config rclone.conf"
 
 # add rclone commands here
 
-${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance                                2>&1 | grep '%'
+${RCLONE} sync -v --delete-before chaofamily-gdrive:cjguidance   wasabi-eu:archive-cjguidance                                2>&1 | grep 'Transferred'
