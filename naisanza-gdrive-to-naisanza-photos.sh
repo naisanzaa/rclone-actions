dba4277f172092c7ed7058edc5823a18757787cf
Eric <58240560+naisanzaa@users.noreply.github.com>
2023-10-03 16:03:40 +0800

    Update naisanza-gdrive-to-naisanza-photos.sh

--- naisanza-gdrive-to-naisanza-photos.sh
+++ naisanza-gdrive-to-naisanza-photos.sh
@@ -6,4 +6,4 @@ RCLONE="rclone --config rclone.conf"
 
 # add rclone commands here
 
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories       2>&1 | grep '%'
+${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories           naisanza-photos:album/Memories       2>&1 | grep 'Transferred'
