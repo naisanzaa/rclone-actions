508d5424ea252ff200396e6f5ee419eb06a850dd
naisanzaa <naisanza@gmail.com>
2025-06-20 05:43:01 -0400

    [scripts] use `copy` after google ruined photos

--- naisanza-gdrive-to-chaofamily-photos.sh
+++ naisanza-gdrive-to-chaofamily-photos.sh
@@ -6,4 +6,4 @@ RCLONE="rclone --config rclone.conf"
 
 # add rclone commands here
 
-${RCLONE} sync -v --delete-before naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life    2>&1 | grep 'Transferred'
+${RCLONE} copy -v naisanza-gdrive:home/eric/pictures/Memories/Life      chaofamily-photos:album/Eric/Life    2>&1 | grep 'Transferred'
